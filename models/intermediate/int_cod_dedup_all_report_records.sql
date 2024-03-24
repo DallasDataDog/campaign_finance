b553176483deb26c16d7b29a2efde0633a412405
Albert Mata <albertmata93@gmail.com>
2024-03-24 20:03:48 +0000

    created final mart layer of dallas campaign finance

--- models/intermediate/int_cod_dedup_all_report_records.sql
+++ models/intermediate/int_cod_dedup_all_report_records.sql
@@ -2,9 +2,10 @@
     SELECT 
         *,
         MD5(CONCAT(full_name, contact_type, amount, candidate_name, transaction_date, street)) AS RECORD_HASH,
-        ROW_NUMBER() OVER(PARTITION BY RECORD_HASH ORDER BY TRANSACTION_DATE DESC) as ROW_NUM
+        ROW_NUMBER() OVER(PARTITION BY RECORD_HASH ORDER BY SOURCE_FILE_NAME DESC) as ROW_NUM
     FROM {{ref("stg_cod__all_report_records")}}
  )
     SELECT *
     FROM ALL_REPORT_RECORDS
-    
+    WHERE ROW_NUM = 1 
+        AND CANDIDATE_NAME != 'COUNCIL MEMBER'
---
+++ models/marts/cod_all_transactions.sql
@@ -0,0 +1,21 @@
+SELECT 
+    ID,
+    RECORD_ID,
+    REPORT_ID,
+    FILE_LINK,
+    FULL_NAME,
+    CONTACT_TYPE,
+    RECORD_TYPE,
+    AMOUNT,
+    SCHEDULE_TYPE,
+    CANDIDATE_NAME,
+    ELECTION_DATE,
+    TRANSACTION_DATE,
+    FULL_ADDRESS,
+    STREET,
+    CITY,
+    STATE,
+    ZIPCODE,
+    GEO_LOCATION_LATITUDE,
+    GEO_LOCATION_LONGITUDE
+FROM {{ref("int_cod_dedup_all_report_records")}}
